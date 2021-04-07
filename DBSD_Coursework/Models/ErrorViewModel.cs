e348f3eef5c72e79c4c16409d9142e6c9620a0fb
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-07 17:44:41 +0500

    Add project files.

---
+++ DBSD_Coursework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DBSD_Coursework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
