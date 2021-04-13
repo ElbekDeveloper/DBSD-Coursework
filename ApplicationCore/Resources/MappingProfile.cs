6ad509fae8fb4ce1483ea2545064cebdd915fdaf
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-13 14:40:30 +0500

    Configure automapper

---
+++ ApplicationCore/Resources/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Domain.Models;
+
+namespace ApplicationCore.Resources
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, GetProductResource>()
+                        .ForMember(gproduct => gproduct.MeasurementUnit, opt => opt.MapFrom(p => p.MeasurementUnit.Name));
+            CreateMap<Manufacturer, ManufacturerResource>().ReverseMap();
+        }
+    }
+}
