ee54e9be80fbd2d768438010da09b2aa1c5eb4d9
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-13 14:38:46 +0500

    Implement repository pattern

---
+++ ApplicationCore/Interfaces/RepositoryInterfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces.RepositoryInterfaces
+{
+    public interface IRepository<T> where T: BaseEntity
+    {
+        Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<int> CreateAsync(T entity, CancellationToken cancellationToken = default);
+        Task<int> UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task<int> DeleteAsync(T entity, CancellationToken cancellationToken = default);
+    }
+}
