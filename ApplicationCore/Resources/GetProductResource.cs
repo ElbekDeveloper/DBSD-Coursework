93a8c2e4dcb7bd413a227ec1caaf981788820604
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-13 14:40:58 +0500

    Add data transfer object (dto)

---
+++ ApplicationCore/Resources/GetProductResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Resources
+{
+    public class GetProductResource
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime ManufacturedDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public ManufacturerResource Manufacturer { get; set; }
+        public string  MeasurementUnit { get; set; }
+    }
+}
