1789214571320588d5f2000dfcc822cb5ef2a1a1
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-13 14:39:45 +0500

    Implement service layer

---
+++ ApplicationCore/Interfaces/ServiceInterfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationCore.Resources;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces.ServiceInterfaces
+{
+    public interface IProductService
+    {
+        Task<List<GetProductResource>> GetAllProductsAsync(CancellationToken cancellationToken = default);
+    }
+}
