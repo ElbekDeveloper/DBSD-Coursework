3dd52b505aab3badd8170ae3e786da9852b64fce
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 05:33:46 +0500

    Add stored procedure for adding record

---
+++ SqlProject/dbo/Stored Procedures/spProduct_Insert.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[spProduct_Insert]
+        @Name nvarchar(255),
+        @Description nvarchar(max),
+        @Price decimal,
+        @ManufacturedDate date,
+        @ExpirationDate date,
+        @ManufacturerId int,
+        @MeasurementUnitId int,
+        @QuantityAtWarehouse int
+AS
+begin
+     INSERT INTO dbo.Product 
+     (
+         Name 
+         ,Description 
+         ,Price 
+         ,ManufacturedDate
+         ,ExpirationDate
+         ,ManufacturerId
+         ,MeasurementUnitId,
+         QuantityAtWarehouse
+     )
+     VALUES
+          (@Name,
+        @Description,
+        @Price,
+        @ManufacturedDate,
+        @ExpirationDate,
+        @ManufacturerId,
+        @MeasurementUnitId,
+		@QuantityAtWarehouse);
+ end
+
