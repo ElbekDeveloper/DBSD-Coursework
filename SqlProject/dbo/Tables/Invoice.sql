2bcabb7d8e4a43a613812a4d7bbf6f7d29f4a1b6
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 15:09:47 +0500

    Create tables

---
+++ SqlProject/dbo/Tables/Invoice.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Invoice] (
+    [Id]                 INT          NOT NULL IDENTITY,
+    [CreatedDate]        DATE         NULL,
+    [ConfirmationStatus] BIT          NULL,
+    [TotalCost]          DECIMAL (18) NULL,
+    [CreatedStaffId]     INT          NULL,
+    [AgentId]            INT          NULL,
+    [WarehouseId]            INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_AgentId] FOREIGN KEY ([AgentId]) REFERENCES [dbo].[CounterAgent] ([Id]),
+    CONSTRAINT [FK_CreatedStaffMemberId] FOREIGN KEY ([CreatedStaffId]) REFERENCES [dbo].[StaffMember] ([Id]),
+    CONSTRAINT [FK_WarehouseId] FOREIGN KEY ([WarehouseId]) REFERENCES [dbo].[Warehouse] ([Id])
+);
+
---
+++ SqlProject/dbo/Tables/InvoiceProduct.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[InvoiceProduct] (
+    [ProductId]    INT          NOT NULL,
+    [InvoiceId]    INT          NOT NULL,
+    [SoldPrice]    DECIMAL (18) NULL,
+    [SoldQuantity] DECIMAL (18) NULL,
+    CONSTRAINT [PK_ProductInvoiceKey] PRIMARY KEY CLUSTERED ([ProductId] ASC, [InvoiceId] ASC),
+    CONSTRAINT [FK_InvoiceId] FOREIGN KEY ([InvoiceId]) REFERENCES [dbo].[Invoice] ([Id]),
+    CONSTRAINT [FK_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Product] ([Id])
+);
+
