1b135de65355d3e3d2986d2109b5f6c06359f9d0
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 19:17:32 +0500

    Create models for invoice

---
+++ Domain/Models/Invoice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public class Invoice:BaseEntity
+    {
+        //To avoid null reference exception 
+        //initialize custom types in constructor
+        public Invoice()
+        {
+            CreatedStaff = new StaffMember();
+            CounterAgent = new CounterAgent();
+            Warehouse = new Warehouse();
+            Products = new List<InvoiceProduct>();
+        }
+        public int InvoiceId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool ConfirmationStatus { get; set; }
+        public decimal TotalCost { get; set; }
+        public StaffMember CreatedStaff { get; set; }
+        public CounterAgent CounterAgent { get; set; }
+        public Warehouse Warehouse { get; set; }
+        public List<InvoiceProduct> Products { get; set; }
+    }
+}
---
+++ Domain/Models/InvoiceProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class InvoiceProduct:BaseEntity
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime ManufacturedDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string Manufacturer { get; set; }
+        public string MeasurementUnit { get; set; }
+        public decimal SoldPrice { get; set; }
+        public int SoldQuantity { get; set; }
+    }
+}
