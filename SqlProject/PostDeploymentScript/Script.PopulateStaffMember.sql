23255233fff1bfddbaaf9703327d5117d12aeefb
Restyled.io <commits@restyled.io>
2021-04-15 10:13:44 +0000

    Restyled by pg_format

--- SqlProject/PostDeploymentScript/Script.PopulateStaffMember.sql
+++ SqlProject/PostDeploymentScript/Script.PopulateStaffMember.sql
@@ -1,14 +1,27 @@
-﻿delete from dbo.StaffMember;
+DELETE FROM dbo.StaffMember;
+
 go
 --insert into staff member table
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Leena', 'Kristoffersen', '764 Sachtjen Crossing', 'lkristoffersen3@123-reg.co.uk', '10/21/2018', '931245367');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Yetty', 'Littlepage', '97 Ridge Oak Trail', 'ylittlepage2@craigslist.org', '8/15/2014', '931245337');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Darren', 'Mines', '422 Comanche Trail', 'dmines7@creativecommons.org', '10/6/2012', '931242367');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Park', 'Sambells', '827 Dottie Point', 'psambells5@domainmarket.com', '8/22/2011', '931245367');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Killy', 'Jerschke', '77359 Fallview Lane', 'kjerschke8@uiuc.edu', '3/29/2015', '931245307');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Marigold', 'Launder', '606 Harbort Pass', 'mlaunder1@nature.com', '8/1/2012', '931240367');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Marietta', 'Bemand', '6873 Everett Trail', 'mbemand4@flickr.com', '9/1/2013', '931245367');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Chrissy', 'Fouldes', '76 Fulton Alley', 'cfouldes0@forbes.com', '5/21/2012', '931245377');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Tabbie', 'Merrgan', '480 Grasskamp Point', 'tmerrgan9@mapy.cz', '3/17/2013', '931215367');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Robeina', 'Petricas', '3 Huxley Wakle', 'rpetri21@ifeng.com', '6/4/2014', '911245067');
-insert into dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber) values ( 'Reina', 'Petri', '3 Huxley Alley', 'rpetri6@ifeng.com', '5/4/2014', '931245067');
+
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Leena', 'Kristoffersen', '764 Sachtjen Crossing', 'lkristoffersen3@123-reg.co.uk', '10/21/2018', '931245367');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Yetty', 'Littlepage', '97 Ridge Oak Trail', 'ylittlepage2@craigslist.org', '8/15/2014', '931245337');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Darren', 'Mines', '422 Comanche Trail', 'dmines7@creativecommons.org', '10/6/2012', '931242367');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Park', 'Sambells', '827 Dottie Point', 'psambells5@domainmarket.com', '8/22/2011', '931245367');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Killy', 'Jerschke', '77359 Fallview Lane', 'kjerschke8@uiuc.edu', '3/29/2015', '931245307');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Marigold', 'Launder', '606 Harbort Pass', 'mlaunder1@nature.com', '8/1/2012', '931240367');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Marietta', 'Bemand', '6873 Everett Trail', 'mbemand4@flickr.com', '9/1/2013', '931245367');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Chrissy', 'Fouldes', '76 Fulton Alley', 'cfouldes0@forbes.com', '5/21/2012', '931245377');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Tabbie', 'Merrgan', '480 Grasskamp Point', 'tmerrgan9@mapy.cz', '3/17/2013', '931215367');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Robeina', 'Petricas', '3 Huxley Wakle', 'rpetri21@ifeng.com', '6/4/2014', '911245067');
+INSERT INTO dbo.StaffMember (FirstName, LastName, Address, Email, RegisterDate, PhoneNumber)
+        VALUES ('Reina', 'Petri', '3 Huxley Alley', 'rpetri6@ifeng.com', '5/4/2014', '931245067');
--- SqlProject/dbo/Tables/Warehouse.sql
+++ SqlProject/dbo/Tables/Warehouse.sql
@@ -1,6 +1,7 @@
-﻿CREATE TABLE [dbo].[Warehouse] (
-    [Id]      INT           IDENTITY (1, 1) NOT NULL,
-    [Address] VARCHAR (255) NULL,
+CREATE TABLE[dbo].[Warehouse] (
+[Id] INT IDENTITY (1,
+        1) NOT NULL,
+[Address] VARCHAR(255) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
