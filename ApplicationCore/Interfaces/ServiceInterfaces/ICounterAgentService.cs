21d199cca8039594cbebf097855acb61c0886ce5
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 15:38:44 +0500

    Add service interfaces

---
+++ ApplicationCore/Interfaces/ServiceInterfaces/ICounterAgentService.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationCore.Resources;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces.ServiceInterfaces
+{
+    public interface ICounterAgentService
+    {
+        Task<List<CounterAgentResource>> GetAllCounterAgentsAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ ApplicationCore/Interfaces/ServiceInterfaces/IStaffMemberService.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationCore.Resources;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces.ServiceInterfaces
+{
+    public interface IStaffMemberService
+    {
+        Task<List<StaffMemberResource>> GetAllStaffMembersAsync(CancellationToken cancellationToken = default);
+    }
+}
