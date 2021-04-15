50ba663142b9643daec34815ebc1af368d6c5347
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 16:04:39 +0500

    Add stored procedures to retrieve records from tables

---
+++ SqlProject/dbo/Stored Procedures/spStaffMember_GetAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spStaffMember_GetAll]
+AS
+BEGIN 
+	SELECT [Id] as StaffMemberId, [FirstName], [LastName], [Address], [Email], [RegisterDate], [PhoneNumber] from dbo.StaffMember;
+END
+
---
+++ SqlProject/dbo/Stored Procedures/spWarehouse_GetAll.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[spWarehouse_GetAll]
+AS
+BEGIN 
+	SELECT [Id] as WarehouseId, [Address] from dbo.Warehouse;
+END
