65264c47d1d3f57e34c006f563acc0529b2a8720
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 19:16:27 +0500

    Add resources for invoice object

---
+++ ApplicationCore/Resources/GetInvoiceProductResource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ApplicationCore.Resources
+{
+    public class GetInvoiceProductResource
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime ManufacturedDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string Manufacturer { get; set; }
+        public string MeasurementUnit { get; set; }
+        public decimal SoldPrice { get; set; }
+        public int SoldQuantity { get; set; }
+
+    }
+}
