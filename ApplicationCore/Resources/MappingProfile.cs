d2672b793df31c9d9886c5cb0560b9240fcb1b35
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 06:50:15 +0500

    Add new configuration for inserting record

--- ApplicationCore/Resources/MappingProfile.cs
+++ ApplicationCore/Resources/MappingProfile.cs
@@ -10,7 +10,13 @@ namespace ApplicationCore.Resources
             CreateMap<Product, GetProductResource>()
                         .ForMember(gproduct => gproduct.MeasurementUnit, opt => opt.MapFrom(p => p.MeasurementUnit.Name));
             CreateMap<Manufacturer, ManufacturerResource>().ReverseMap();
-            CreateMap<AddProductResource, Product>();
+            CreateMap<Product, AddProductResource>()
+                        .ForMember(p => p.ManufacturerId, opt => opt.MapFrom(pr => pr.Manufacturer.ManufacturerId))
+                        .ForMember(p => p.ManufacturerId, opt => opt.MapFrom(pr => pr.MeasurementUnit.MeasurementUnitId))
+                        .ForMember(p => p.QuantityAtWarehouse, opt => opt.MapFrom(pr => pr.QuantityAtWarehouse))
+                        .ReverseMap();
+
+
         }
     }
 }
