3ae5343630fff9a348d414d88605911a118b1dbd
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 16:32:05 +0500

    Add controllers

---
+++ WebApi/Controllers/CounterAgentsController.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationCore.Interfaces.ServiceInterfaces;
+using ApplicationCore.Resources;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class CounterAgentsController : ControllerBase
+    {
+        private readonly ICounterAgentService _counterAgentService;
+
+        public CounterAgentsController(ICounterAgentService counterAgentService)
+        {
+            _counterAgentService = counterAgentService;
+        }
+
+
+        [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "All Counter Agents", Type = typeof(List<CounterAgentResource>))]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult<IEnumerable<CounterAgentResource>>> GetManufacturers(CancellationToken cancellationToken = default)
+        {
+            return Ok(await _counterAgentService.GetAllCounterAgentsAsync(cancellationToken));
+        }
+    }
+}
---
+++ WebApi/Controllers/WarehouseController.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationCore.Interfaces.ServiceInterfaces;
+using ApplicationCore.Resources;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+        private readonly IWarehouseService _warehouseService;
+
+        public WarehouseController(IWarehouseService warehouseService)
+        {
+            _warehouseService = warehouseService;
+        }
+
+
+        [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "All Warehouses", Type = typeof(List<WarehouseResource>))]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult<IEnumerable<WarehouseResource>>> GetManufacturers(CancellationToken cancellationToken = default)
+        {
+            return Ok(await _warehouseService.GetAllWarehousesAsync(cancellationToken));
+        }
+    }
+}
