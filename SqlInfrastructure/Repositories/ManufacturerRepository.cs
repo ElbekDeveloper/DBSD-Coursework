d91f14c7a1f339cb42c62b5f7ba7dc958a4bc06a
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 11:42:48 +0500

    Impelement manufacturer interface

---
+++ SqlInfrastructure/Repositories/ManufacturerRepository.cs
@@ -0,0 +1,57 @@
+﻿using ApplicationCore.Interfaces.RepositoryInterfaces;
+using Dapper;
+using Domain.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SqlInfrastructure.Repositories
+{
+    public class ManufacturerRepository : BaseRepository, IManufacturerRepository
+    {
+        public ManufacturerRepository(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public Task<int> CreateAsync(Manufacturer entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(Manufacturer entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Manufacturer>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                string procedure = "spManufacturer_GetAll";
+                using (var connection = CreateConnection())
+                {
+                    return (await connection.QueryAsync<Manufacturer>(sql: procedure, commandType: CommandType.StoredProcedure)).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        public Task<Manufacturer> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(Manufacturer entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
