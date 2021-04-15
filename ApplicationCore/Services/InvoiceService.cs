afa86ae24178637e26f4081088f99c2350adb9e9
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-15 19:17:17 +0500

    Retrive Invoice

---
+++ ApplicationCore/Services/InvoiceService.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationCore.Interfaces.RepositoryInterfaces;
+using ApplicationCore.Interfaces.ServiceInterfaces;
+using ApplicationCore.Resources;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class InvoiceService : IInvoiceService
+    {
+        private readonly IInvoiceRepository _invoiceRepository;
+        private readonly IMapper _mapper;
+
+        public InvoiceService(IInvoiceRepository invoiceRepository, IMapper mapper)
+        {
+            _invoiceRepository = invoiceRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetInvoiceResource>> GetAllInvoicesAsync(CancellationToken cancellationToken = default)
+        {
+            var invoices = await _invoiceRepository.GetAllAsync(cancellationToken);
+            var invoiceResources = _mapper.Map<List<GetInvoiceResource>>(invoices);
+
+            return invoiceResources;
+        }
+    }
+}
