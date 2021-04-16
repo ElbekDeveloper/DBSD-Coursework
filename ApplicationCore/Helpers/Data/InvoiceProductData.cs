d1646f504951d14ca268d8f3338a11003968d90b
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-16 07:23:18 +0500

    Add some helper classes

---
+++ ApplicationCore/Helpers/Data/InvoiceProductData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Helpers.Data
+{
+    public class InvoiceProductData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal CalculatedPrice { get; set; }
+        public decimal Profit { get; set; }
+    }
+}
