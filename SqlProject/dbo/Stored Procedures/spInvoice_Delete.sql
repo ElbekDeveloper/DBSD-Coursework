ec6e47f717173254118291a313b4d4a6f55a5d14
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-04-16 08:10:12 +0500

    Create stored procedure for delte

---
+++ SqlProject/dbo/Stored Procedures/spInvoice_Delete.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spInvoice_Delete]
+	@InvoiceId int
+AS
+begin 
+	delete from dbo.InvoiceProduct
+	where InvoiceId=@InvoiceId;
+
+	delete from dbo.Invoice
+	where Id=@InvoiceId;
+end
